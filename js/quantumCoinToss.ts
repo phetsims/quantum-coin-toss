d89e4784d2cdcab23978c800c2ec802e79c7e9ed
jbphet <jpblanco@yahoo.com>
2025-03-04 17:35:43 -0700

    remove files that were auto-generated during repo setup that are no longer needed

--- js/quantumCoinToss.ts
+++ js/quantumCoinToss.ts
@@ -3,7 +3,7 @@
 /**
  * Creates the namespace for this simulation.
  *
- * @author John
+ * @author John Blanco (PhET Interactive Simulations)
  */
 
 import Namespace from '../../phet-core/js/Namespace.js';
