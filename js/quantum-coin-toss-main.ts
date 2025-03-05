c8322cacaa0f595361f76576e98524ad0cad173c
jbphet <jpblanco@yahoo.com>
2025-03-04 17:16:46 -0700

    set up this sim to extract content from quantum-measurement

--- js/quantum-coin-toss-main.ts
+++ js/quantum-coin-toss-main.ts
@@ -3,26 +3,24 @@
 /**
  * Main entry point for the sim.
  *
- * @author John
+ * @author John Blanco (PhET Interactive Simulations)
  */
 
 import Sim, { SimOptions } from '../../joist/js/Sim.js';
 import simLauncher from '../../joist/js/simLauncher.js';
+import CoinsScreen from '../../quantum-measurement/js/coins/CoinsScreen.js';
 import Tandem from '../../tandem/js/Tandem.js';
-import QuantumCoinTossScreen from './quantum-coin-toss/QuantumCoinTossScreen.js';
 import QuantumCoinTossStrings from './QuantumCoinTossStrings.js';
 import './common/QuantumCoinTossQueryParameters.js';
 
 // Launch the sim. Beware that scenery Image nodes created outside simLauncher.launch() will have zero bounds
 // until the images are fully loaded. See https://github.com/phetsims/coulombs-law/issues/70#issuecomment-429037461
 simLauncher.launch( () => {
-
   const titleStringProperty = QuantumCoinTossStrings[ 'quantum-coin-toss' ].titleStringProperty;
-
   const screens = [
-    new QuantumCoinTossScreen( { tandem: Tandem.ROOT.createTandem( 'quantumCoinTossScreen' ) } )
+    // new QuantumCoinTossScreen( { tandem: Tandem.ROOT.createTandem( 'quantumCoinTossScreen' ) } )
+    new CoinsScreen( Tandem.ROOT.createTandem( 'coinsScreen' ) )
   ];
-
   const options: SimOptions = {
 
     //TODO fill in credits, all of these fields are optional, see joist.CreditsNode, see https://github.com/phetsims/quantum-measurement/issues/1
