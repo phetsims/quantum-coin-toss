aebc8a111538742b038bad82141db10db1e3efae
Michael Kauzmann <michael.kauzmann@colorado.edu>
2025-03-25 16:33:00 -0600

    runnable *QueryParameters.ts files to use QueryStringMachineModule, https://github.com/phetsims/query-string-machine/issues/54

--- js/common/QuantumCoinTossQueryParameters.ts
+++ js/common/QuantumCoinTossQueryParameters.ts
@@ -8,6 +8,7 @@
  */
 
 import logGlobal from '../../../phet-core/js/logGlobal.js';
+import { QueryStringMachine } from '../../../query-string-machine/js/QueryStringMachineModule.js';
 import quantumCoinToss from '../quantumCoinToss.js';
 
 const SCHEMA_MAP = {
