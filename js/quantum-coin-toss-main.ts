835db33570382592c1ff8f27d7f3e02d1243a139
jbphet <jpblanco@yahoo.com>
2025-03-26 17:37:04 -0600

    add support for configurable preferences dialog and integrate into QCT, see https://github.com/phetsims/quantum-measurement/issues/133

--- js/quantum-coin-toss-main.ts
+++ js/quantum-coin-toss-main.ts
@@ -6,10 +6,12 @@
  * @author John Blanco (PhET Interactive Simulations)
  */
 
+import PreferencesModel from '../../joist/js/preferences/PreferencesModel.js';
 import Sim, { SimOptions } from '../../joist/js/Sim.js';
 import simLauncher from '../../joist/js/simLauncher.js';
 import CoinsScreen from '../../quantum-measurement/js/coins/CoinsScreen.js';
 import QuantumMeasurementConstants from '../../quantum-measurement/js/common/QuantumMeasurementConstants.js';
+import QuantumMeasurementPreferencesNode from '../../quantum-measurement/js/common/view/QuantumMeasurementPreferencesNode.js';
 import Tandem from '../../tandem/js/Tandem.js';
 import QuantumCoinTossStrings from './QuantumCoinTossStrings.js';
 import './common/QuantumCoinTossQueryParameters.js';
@@ -17,13 +19,27 @@ import './common/QuantumCoinTossQueryParameters.js';
 // Launch the sim. Beware that scenery Image nodes created outside simLauncher.launch() will have zero bounds
 // until the images are fully loaded. See https://github.com/phetsims/coulombs-law/issues/70#issuecomment-429037461
 simLauncher.launch( () => {
+
   const titleStringProperty = QuantumCoinTossStrings[ 'quantum-coin-toss' ].titleStringProperty;
+
   const screens = [
     new CoinsScreen( Tandem.ROOT.createTandem( 'coinsScreen' ) )
   ];
+
   const options: SimOptions = {
-    credits: QuantumMeasurementConstants.CREDITS
+    credits: QuantumMeasurementConstants.CREDITS,
+    preferencesModel: new PreferencesModel( {
+      simulationOptions: {
+        customPreferences: [ {
+          createContent: tandem => new QuantumMeasurementPreferencesNode( {
+            tandem: tandem.createTandem( 'simPreferences' ),
+            hasBlochSphereNumericalEquation: false
+          } )
+        } ]
+      }
+    } )
   };
+
   const sim = new Sim( titleStringProperty, screens, options );
   sim.start();
 } );
